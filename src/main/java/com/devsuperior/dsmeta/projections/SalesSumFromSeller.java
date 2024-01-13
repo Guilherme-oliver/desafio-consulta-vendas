39a18442d0ac3da8257800b155bd06dd13a20712
Guilherme <guilherme.rodriguess.oliveira@gmail.com>
2024-01-12 21:46:59 -0300

    Update

--- src/main/java/com/devsuperior/dsmeta/projections/SalesSumFromSeller.java
+++
@@ -1,7 +0,0 @@
-package com.devsuperior.dsmeta.projections;
-
-public interface SalesSumFromSeller {
-
-    String getSellerName();
-    Double getTotal();
-}
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,22 +1,18 @@
 package com.devsuperior.dsmeta.services;
 
 import java.time.LocalDate;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import com.devsuperior.dsmeta.dto.SalesWithSellerReportDTO;
-import com.devsuperior.dsmeta.entities.Seller;
-import com.devsuperior.dsmeta.repositories.SellerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -34,66 +30,20 @@ public class SaleService {
 
 	@Transactional(readOnly = true)
 	public List<SalesWithSellerReportDTO> getSalesReportForThePeriodAndSeller(LocalDate minDate, LocalDate maxDate, String sellerName) {
-		if (minDate == null) {
-			minDate = LocalDate.now().minusYears(1);
-		}
-
-		if (maxDate == null) {
-			maxDate = LocalDate.now();
-		}
-		List<Sale> salesReport = repository.getSalesReportForThePeriodAndSeller(minDate, maxDate, sellerName);
+		LocalDate adjustedMinDate = (minDate == null) ? LocalDate.now().minusYears(1) : minDate;
+		LocalDate adjustedMaxDate = (maxDate == null) ? LocalDate.now() : maxDate;
+		List<Sale> salesReport = repository.getSalesReportForThePeriodAndSeller(adjustedMinDate, adjustedMaxDate, sellerName);
 		return salesReport.stream()
-				.map(sale -> new SalesWithSellerReportDTO(sale))
-				.collect(Collectors.toList()
-		);
+				.map(SalesWithSellerReportDTO::new)
+				.collect(Collectors.toList());
 	}
 
 	@Transactional(readOnly = true)
 	public List<SaleSummaryDTO> getSalesSummaryOfSalesBySalesPersonInThePeriod(LocalDate minDate, LocalDate maxDate) {
-		if (minDate == null) {
-			minDate = LocalDate.now().minusYears(1);
-		}
+		LocalDate adjustedMinDate = (minDate == null) ? LocalDate.now().minusYears(1) : minDate;
+		LocalDate adjustedMaxDate = (maxDate == null) ? LocalDate.now() : maxDate;
 
-		if (maxDate == null) {
-			maxDate = LocalDate.now();
-		}
-		List<SaleSummaryDTO> result =
-				repository.getSalesSummaryOfSalesBySalesPersonInThePeriod(minDate, maxDate);
-		return result.stream().collect(Collectors.toList());
+		List<SaleSummaryDTO> salesSummary = repository.getSalesSummaryOfSalesBySalesPersonInThePeriod(adjustedMinDate, adjustedMaxDate);
+		return salesSummary.stream().collect(Collectors.toList());
 	}
-
-	/*
-	@Transactional(readOnly = true)
-	public List<SaleSummaryDTO> getSalesSummaryOfSalesBySalesPersonInThePeriod(LocalDate minDate, LocalDate maxDate) {
-		if (minDate == null) {
-			minDate = LocalDate.now().minusYears(1);
-		}
-
-		if (maxDate == null) {
-			maxDate = LocalDate.now();
-		}
-		List<Seller> salesSummary = repository.getSalesSummaryOfSalesBySalesPersonInThePeriod(minDate, maxDate);
-		return salesSummary.stream()
-				.map(sale -> new SaleSummaryDTO(sale))
-				.collect(Collectors.toList()
-		);
-	}
-
-
-	@Transactional(readOnly = true)
-	public List<SalesFromSellerDTO> getSalesReportByDateRangeAndSeller(LocalDate minDate, LocalDate maxDate, String sellerName) {
-		maxDate = LocalDate.now();
-		minDate = maxDate.minusYears(1);
-		List<Sale> result = repository.getSalesReportByDateRangeAndSeller(minDate, maxDate, sellerName);
-		return result.stream()
-				.map(sale -> new SalesFromSellerDTO(sale))
-				.collect(Collectors.toList()
-		);
-	}
-
-	private String converterLocalDateParaString(LocalDate localDate) {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-		return localDate.format(formatter);
-	}
-	 */
 }
